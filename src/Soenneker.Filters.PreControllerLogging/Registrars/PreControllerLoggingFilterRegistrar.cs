5f68c92bc998a6ddfa479e1fd30e9e52db297078
soenneker <jake@soenneker.com>
2026-08-29 14:17:03 -0500

    Improve public API XML documentation

--- src/Soenneker.Filters.PreControllerLogging/Registrars/PreControllerLoggingFilterRegistrar.cs
+++ src/Soenneker.Filters.PreControllerLogging/Registrars/PreControllerLoggingFilterRegistrar.cs
@@ -10,6 +10,8 @@ public static class PreControllerLoggingFilterRegistrar
     /// <summary>
     /// Adds a new <see cref="PreControllerLoggingFilterAttribute"/> to the filter collection<para/>
     /// </summary>
+    /// <param name="filterCollection">filter Collection to inspect or update.</param>
+    /// <returns>The resulting filter Collection.</returns>
     public static FilterCollection AddPreControllerLoggingFilter(this FilterCollection filterCollection)
     {
         filterCollection.Add(new PreControllerLoggingFilterAttribute());
